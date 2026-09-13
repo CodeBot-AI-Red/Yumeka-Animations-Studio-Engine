6898aad7f3436a0919663e9426e309e135e7df81
CodeBot-AI-Red <zestfrosty@gmail.com>
2026-09-13 17:04:59 -0300

    refactor: migra EditorFragment para com.yumeka.anime.engine.fragments

---
+++ android/app/src/main/kotlin/com/yumeka/anime/engine/fragments/EditorFragment.kt
@@ -0,0 +1,17 @@
+package com.yumeka.anime.engine.fragments
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.yumeka.anime.engine.R
+
+class EditorFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.fragment_editor, container, false)
+    }
+}
