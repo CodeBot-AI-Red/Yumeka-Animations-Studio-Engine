cb4f4fd6e6d603e6bdd43e06dc1c1c86419b2a51
CodeBot-AI-Red <zestfrosty@gmail.com>
2026-09-13 12:06:32 -0300

    fix: remove kotlin(android) duplicado e kotlinOptions legado

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -34,8 +34,10 @@ android {
         targetCompatibility = JavaVersion.VERSION_17
     }
 
-    kotlinOptions {
-        jvmTarget = "17"
+    kotlin {        // KGP 2.x - substitui kotlinOptions
+        compilerOptions {
+            jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_17)
+        }
     }
 
     buildFeatures {
@@ -48,24 +50,21 @@ dependencies {
     implementation("androidx.core:core-ktx:1.12.0")
     implementation("androidx.appcompat:appcompat:1.6.1")
     implementation("com.google.android.material:material:1.11.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
+    implementation( "androidx.constraintlayout:constraintlayout:2.1.4")
     implementation("androidx.fragment:fragment-ktx:1.6.2")
     implementation("androidx.recyclerview:recyclerview:1.3.2")
 
-    // Coroutines & Lifecycle
-    implementation("org.jetbrains.kotlinx.coroutines-core:1.7.3")
-    implementation("org.jetbrains.kotlinx.coroutines-android:1.7.3")
+    // Coroutines
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.3")
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.3")
     implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.7.0")
 
-    // Rendering 2D - Animação anime
-    implementation("com.github.airbnb:lottie-android:6.3.0")
+    // Lottie - animações vetoriais
+    implementation("com.airbnb:lottie:6.3.0")
 
-    // Carregamento de imagens / sprites
-    implementation("io.coil-kt:coil:2.5.0")
+    // Coil - sprites
+    implementation( "io.coil-kt:coil:2.5.0")
 
-    // Rede - streaming de animações remotas
+    // OkHttp
     implementation("com.squareup.okhttp:okhttp:4.12.0")
-
-    // Kotlin stdlib
-    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.9.20")
 }
