55f2d69703aa7fcffa93335a4b8d4c36276ffce9
CodeBot-AI-Red <zestfrosty@gmail.com>
2026-09-13 17:08:39 -0300

    refactor: migra Project model para com.yumeka.anime.engine.models

---
+++ android/app/src/main/kotlin/com/yumeka/anime/engine/models/Project.kt
@@ -0,0 +1,15 @@
+package com.yumeka.anime.engine.models
+
+import java.io.Serializable
+
+data class Project(
+    val id: String,
+    val name: String,
+    val path: String,
+    val version: String,
+    val lastEdited: String,
+    val iconRes: Int,
+    val isFavorite: Boolean = false,
+    val hasError: Boolean = false,
+    val errorMessage: String? = null
+) : Serializable
